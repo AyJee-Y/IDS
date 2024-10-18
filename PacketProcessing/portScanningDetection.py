4064a18d5a45248b324136dc5692c9364f437391
Yonatan <yonatan.halpern@gmail.com>
2024-10-18 21:28:37 +0300

    [ADD] Added infrastructure to log port scans

--- PacketProcessing/portScanningDetection.py
+++ PacketProcessing/portScanningDetection.py
@@ -4,6 +4,7 @@ from scapy.all import IP, TCP, UDP
 import threading
 
 connections = defaultdict(lambda: defaultdict(list))
+portScans = defaultdict(list)
 
 PORT_SCAN_THRESHOLD = 200
 PORT_SCAN_TIME_WINDOW = 300
@@ -28,7 +29,12 @@ def analyze_packet(packet):
 
             unique_ports = set(port for port, _ in connections[src_ip][dst_ip])
             if len(unique_ports) >= PORT_SCAN_THRESHOLD:
-                print(f"PORT SCAN - FROM {dst_ip}")
+                if (src_ip in portScans.keys()):
+                    portScans[src_ip][0] = portScans[src_ip][0] + 1
+                    portScans[src_ip][2] = currentTime
+                else:
+                    portScans[src_ip] = [len(unique_ports), currentTime, currentTime]
+                    print(f"PORT SCAN - FROM {dst_ip}")
 
 def cleanUpOldPackets(stop_event):
     while not stop_event.is_set():
@@ -42,12 +48,16 @@ def performCleanup():
             for dst_ip in list(connections[src_ip].keys()):
                 connections[src_ip][dst_ip] = [
                     (port, timestamp) for port, timestamp in connections[src_ip][dst_ip]
-                    if current_time - timestamp <= (CLEANUP_INTERVAL * 2)
+                    if current_time - timestamp <= (CLEANUP_INTERVAL * 1.5)
                 ]
                 if not connections[src_ip][dst_ip]:
                     del connections[src_ip][dst_ip]
             if not connections[src_ip]:
                 del connections[src_ip]
+        for src_ip in list(portScans.keys()):
+            if (current_time - portScans[src_ip][2] <= (CLEANUP_INTERVAL * 2)):
+                print(f"PORT SCAN FROM {src_ip}: CHECKED {portScans[src_ip][0]} PORTS")
+                del portScans[src_ip]
 
 def detectPortScan(packet):
     analyze_packet(packet)
